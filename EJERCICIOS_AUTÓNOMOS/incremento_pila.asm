c60abf4e01738738a87eecc55f06d92e05f9e2eb
Yulissa Stefania <33470004+yulissatq@users.noreply.github.com>
2019-07-28 13:41:32 -0500

    Add files via upload

---
+++ EJERCICIOS_AUTÓNOMOS/incremento_pila.asm
@@ -0,0 +1,68 @@
+section .data
+
+    num1 db "Ingrese un numero: "
+    len1 equ $-num1
+
+    nlin db 10, " "
+
+section .bss
+    num resb 1
+    resultado resb 1
+    
+section .text
+    global _start
+    
+_start:
+    mov eax, 4 
+    mov ebx, 1
+    mov ecx, num1
+    mov edx, len1
+    int 80H
+    
+    mov eax, 3
+    mov ebx, 2
+    mov ecx, num
+    mov edx, 2
+    int 80H
+
+    mov al,[num]
+    sub al,'0'
+
+    mov ecx,eax
+
+    mov ebx, 1
+    add ebx, '0'
+    ;push ebx
+
+ciclo:
+
+    push ecx ;5
+    push ebx
+    
+       
+    mov [resultado], ebx
+    
+
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, resultado
+    mov edx, 1
+    int 80H
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, nlin
+    mov edx, 1
+    int 80H
+
+    pop ebx 
+    inc ebx
+    
+    pop ecx ;1,2
+
+    loop ciclo
+
+salir:            
+    mov eax, 1
+    int 80H
